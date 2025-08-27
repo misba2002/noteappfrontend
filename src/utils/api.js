0248d49b4ace2301477db24c8a1176bb80a6cf83
misba2002 <misbajunneidi@gmail.com>
2025-08-27 14:55:45 +0530

    base url update

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const API = axios.create({ baseURL: `${process.env.NEXT_PUBLIC_API_URL}` });
 
 API.interceptors.request.use((req) => {
+ console.log("API baseURL 👉", API.defaults.baseURL);
+
 const tokencurrent = localStorage.getItem("token");
  if (tokencurrent){
   req.headers.Authorization = `Bearer ${tokencurrent}`;
